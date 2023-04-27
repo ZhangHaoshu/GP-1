2dc1c2e72bc4b2981a6039c8171fe93df25132da
ZhangHaoshu <zhs17372071719@gmail.com>
2023-04-27 19:55:52 +0800

    完成了详情页面的开发

--- pages/device_detail/device_detail.js
+++ pages/device_detail/device_detail.js
@@ -1,18 +1,41 @@
 // pages/device_detail/device_detail.js
+// 引入本地数据
+const localData = require('../../testdata/localData.js');
+
+
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        // 环形进度条渐变色
+        gradientColor: {
+            '0%': '#ffd01e',
+            '100%': '#ee0a24',
+        },
+        device_detail: [],
+        ID: 0
     },
+
+    // // 获取对应设备详情信息
+    // getDetail() {
+    //     JSONArray = localData.getJSONArray("list")
+    // },
+
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        var str = options.device_id
+        this.setData({
+            ID: str,
+            device_detail: localData
+        })
+        //console.log(this.id)
+        //this.getDetail()
     },
 
     /**
